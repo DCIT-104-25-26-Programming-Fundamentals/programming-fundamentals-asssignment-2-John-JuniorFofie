7c833fed19836ba2e2291572c2bc428ab4e70416
John-JuniorFofie <johnfofie31@gmail.com>
2026-07-28 03:56:12 +0000

    modified:   assignment_03_array_statistics.cpp

--- assignment_03_array_statistics.cpp
+++ assignment_03_array_statistics.cpp
@@ -42,3 +42,105 @@
 #include <iostream>
 using namespace std;
 
+
+// Function to calculate the sum of all numbers
+int calculateSum(int numbers[], int size)
+{
+    int sum = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        sum += numbers[i];
+    }
+
+    return sum;
+}
+
+
+// Function to calculate the average
+double calculateAverage(int numbers[], int size)
+{
+    int sum = calculateSum(numbers, size);
+
+    return static_cast<double>(sum) / size;
+}
+
+
+// Function to find the maximum number
+int findMaximum(int numbers[], int size)
+{
+    int max = numbers[0];
+
+    for (int i = 1; i < size; i++)
+    {
+        if (numbers[i] > max)
+        {
+            max = numbers[i];
+        }
+    }
+
+    return max;
+}
+
+
+// Function to find the minimum number
+int findMinimum(int numbers[], int size)
+{
+    int min = numbers[0];
+
+    for (int i = 1; i < size; i++)
+    {
+        if (numbers[i] < min)
+        {
+            min = numbers[i];
+        }
+    }
+
+    return min;
+}
+
+
+int main()
+{
+    int n;
+
+    cout << "How many numbers? ";
+    cin >> n;
+
+
+    // Check if number of inputs is valid
+    if (n <= 0)
+    {
+        cout << "Error: Please enter a positive integer." << endl;
+        return 1;
+    }
+
+
+    int numbers[100];
+
+
+    // Getting numbers from user
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter number " << i + 1 << ": ";
+        cin >> numbers[i];
+    }
+
+
+    // Calling functions
+    int sum = calculateSum(numbers, n);
+    double average = calculateAverage(numbers, n);
+    int maximum = findMaximum(numbers, n);
+    int minimum = findMinimum(numbers, n);
+
+
+    // Display results
+    cout << "\nResults:" << endl;
+    cout << "Sum:     " << sum << endl;
+    cout << "Average: " << average << endl;
+    cout << "Maximum: " << maximum << endl;
+    cout << "Minimum: " << minimum << endl;
+
+
+    return 0;
+}
